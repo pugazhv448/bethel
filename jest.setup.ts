3717f889031b43cb14bdc88d53d7e7b61c0b3d81
pugazhv448 <pugazhv1509@gmail.com>
2026-06-26 10:55:08 +0530

    Fix unused parameters and implicit any in jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,9 +2,9 @@ import '@testing-library/jest-dom';
 
 // Mock IntersectionObserver for jsdom environment
 class IntersectionObserver {
-  constructor(callback, options) {}
+  constructor(_callback: any, _options?: any) {}
   observe() {}
   unobserve() {}
   disconnect() {}
 }
-global.IntersectionObserver = IntersectionObserver;
+global.IntersectionObserver = IntersectionObserver as any;
